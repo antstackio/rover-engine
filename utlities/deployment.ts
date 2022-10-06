f19def4e56aa7a2d7965217091c4576ee3c9eecd
Dheerajgbhatt <79825980+DheerajGBhatt@users.noreply.github.com>
2022-10-06 17:21:21 +0530

    python package checking,error handling and proper messaging

--- utlities/deployment.ts
+++ utlities/deployment.ts
@@ -1,19 +1,38 @@
 const exec = require("child_process").execSync;
 const process = require('process');
 let fs = require("fs");
-import { AnyObject } from "immer/dist/internal";
+import { info } from "console";
+import { AnyArray, AnyObject } from "immer/dist/internal";
 import * as rover_config  from "../utlities/config"
 
 export function setupRepo(repoconfig:AnyObject){
-    repoconfig.app_name=exec("pwd").toString().replace("\n","");
-    let filenamearray=( repoconfig.app_name).split("/")
-    repoconfig.name = filenamearray[filenamearray.length-1].replace("\n","");
-    let appname=repoconfig.app_name
-    let repoconfigres:string=JSON.stringify(repoconfig)
-    if(!fs.existsSync(appname+"/.github")) exec("mkdir "+appname+"/.github") 
-    if(!fs.existsSync(appname+"/.github/workflows"))exec("mkdir "+appname+"/.github/workflows") 
-    exec("python3 -m pip install pyyaml")     
-    exec("python3 "+rover_config.npmroot+"/@rover-tools/cli/node_modules/@rover-tools/engine/pipeline/pipelinegenerator.py "+ appname+"/.github/workflows/main.yml "+appname+"/region.txt "+appname+"/accesskey.txt "+appname+"/secret.txt "+"'"+repoconfigres+"'")         
-    process.chdir(appname);
-   
+    try {
+        let piplibraries:string=""
+        try {
+            piplibraries=exec("pip3 list").toString()
+        } catch (error) {
+            throw new Error("Python3 and pip3 required");   
+        }
+        if (piplibraries==="") throw new Error("yaml,sys and json python modules are required.");
+        let piplibrarieslist:AnyArray=[]
+        piplibraries.split(" ").forEach(ele=>{
+            if (ele!=='') piplibrarieslist.push(ele.replace(/[!@#$%^&\*()_\-={}\[\]:\"<>,\\.?\n]*[0-9]*/g, ''))
+
+        })
+        if (piplibraries.includes("pyyaml")) throw new Error("install yaml library (pip3 install pyyaml)")
+        if (piplibraries.includes("sys")) throw new Error("install sys library (pip3 install sys)")
+        
+        repoconfig.app_name=exec("pwd").toString().replace("\n","");
+        let filenamearray=( repoconfig.app_name).split("/")
+        repoconfig.name = filenamearray[filenamearray.length-1].replace("\n","");
+        let appname=repoconfig.app_name
+        let repoconfigres:string=JSON.stringify(repoconfig)
+        if(!fs.existsSync(appname+"/.github")) exec("mkdir "+appname+"/.github") 
+        if(!fs.existsSync(appname+"/.github/workflows"))exec("mkdir "+appname+"/.github/workflows") 
+        exec("python3 -m pip install pyyaml")     
+        exec("python3 "+rover_config.npmroot+"/@rover-tools/cli/node_modules/@rover-tools/engine/pipeline/pipelinegenerator.py "+ appname+"/.github/workflows/main.yml "+appname+"/region.txt "+appname+"/accesskey.txt "+appname+"/secret.txt "+"'"+repoconfigres+"'")         
+        process.chdir(appname);   
+    } catch (error) {
+        throw new Error(error.message)
+    }
 }
